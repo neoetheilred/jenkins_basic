711fdc80d54f27bd10c521a5a045d0048b3a6ac3
roman <rdandrievskiy@edu.hse.ru>
2023-06-29 15:56:01 +0200

    initial

---
+++ src/main/java/com/example/currencyratesapi/CatFactService.java
@@ -0,0 +1,15 @@
+package com.example.currencyratesapi;
+
+import org.springframework.stereotype.Service;
+
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+@Service
+public class CatFactService {
+    public String getFact() {
+        return "";
+    }
+}
---
+++ src/main/java/com/example/currencyratesapi/Greeting.java
@@ -0,0 +1,4 @@
+package com.example.currencyratesapi;
+
+public record Greeting(long id, String name) {
+}
